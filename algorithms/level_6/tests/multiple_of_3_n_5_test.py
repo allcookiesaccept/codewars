d4090e64e7d2f9e4cfd4fd2be2170cd62fc8e79a
allcookiesaccept <insufferable.lobster@gmail.com>
2023-04-14 12:19:18 +0300

    fix: put cases in variables

--- algorithms/level_6/tests/multiple_of_3_n_5_test.py
+++ algorithms/level_6/tests/multiple_of_3_n_5_test.py
@@ -2,8 +2,10 @@ import pytest
 
 from algorithms.level_6.multiple_of_3_n_5 import sum_of_multipliers
 
+params = [(4, 3), (6, 8), (16, 60), (3, 0), (5, 3), (15, 45)]
 
-@pytest.mark.parametrize("number, result",
-                         [(4, 3), (6, 8), (16, 60), (3, 0), (5, 3), (15, 45)])
+ids = ["%s = %s" % (number, result) for (number, result) in params]
+
+@pytest.mark.parametrize(argnames="number, result", argvalues=params, ids=ids)
 def sum_of_multipliers_test(number, result):
     assert sum_of_multipliers(number) == result
